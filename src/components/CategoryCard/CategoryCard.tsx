1414bb4df9546eebe6eb4dd335b29ae70832fc66
Pavel Nikitin <pavel.nikitin@corp.vk.com>
2023-06-26 03:59:10 +0300

    fix categoryCard

--- src/components/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCard/CategoryCard.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Card } from '@vkontakte/vkui'
-import { Icon28StickerOutline } from '@vkontakte/icons'
 import { formatWordByNumber } from 'src/utils/formatByNumbers'
 
 import './CategoryCard.css'
@@ -13,7 +12,7 @@ export type CategoryCardProps = {
 
 let CategoryCard: React.FC<
   CategoryCardProps & React.HtmlHTMLAttributes<HTMLDivElement>
-> = ({ name, productCount, preview, ...props }) => {
+> = ({ name, productCount, ...props }) => {
   return (
     <Card {...props} className="CategoryCard">
       <div className="CategoryCard_info">
@@ -22,11 +21,6 @@ let CategoryCard: React.FC<
           {productCount}{' '}
           {formatWordByNumber(productCount, 'товар', 'товара', 'товаров')}
         </div>
-      </div>
-
-      <div className="CategoryCard_preview">
-        {preview && <img src={preview}></img>}
-        {!preview && <Icon28StickerOutline fill="2688EB" />}
       </div>
     </Card>
   )

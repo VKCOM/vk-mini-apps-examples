e09db900f837bb8d2a57a297f1059205efb02deb
Philip Apanasenko <f.apanasenko@vk.team>
2023-07-17 17:59:13 +0300

    Remove images from bundle

---
+++ src/serviceWorker/controllers/getInitialData.ts
@@ -0,0 +1,38 @@
+import { CATEGORIES, PRODUCTS, IMAGE_DOMAIN } from '../config'
+
+/** Получаем контент стартовой страницы */
+export function getInitialData() {
+  let minPrice = PRODUCTS[0].price
+  let maxPrice = PRODUCTS[0].price
+
+  PRODUCTS.forEach((item) => {
+    if (item.price > maxPrice) maxPrice = item.price
+    if (item.price < minPrice) minPrice = item.price
+  })
+
+  const categories = CATEGORIES.map((category) => {
+    return {
+      ...category,
+      productCount: PRODUCTS.reduce((res, item) => {
+        if (item.categoryId.includes(category.id)) return res + 1
+        return res
+      }, 0),
+    }
+  })
+
+  const shuffled = PRODUCTS.sort(() => 0.5 - Math.random())
+
+  const shopInfo = {
+    name: 'VK Магазин',
+    logo: IMAGE_DOMAIN + 'logo.png',
+    maxPrice,
+    minPrice,
+  }
+  return new Response(
+    JSON.stringify({
+      recommendedProducts: shuffled.slice(0, 9),
+      categories,
+      shopInfo,
+    })
+  )
+}
